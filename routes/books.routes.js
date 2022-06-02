fc79540640cacb7afe3c6382dd41830a07638b08
AmirAliEidivandi <amirah648887@gmail.com>
2022-06-02 19:03:59 +0430

    UPDATE Book by id

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -36,8 +36,38 @@ router.get("/:id", async (req, res) => {
     try {
         const { id: bookId } = req.params;
         const book = await Book.findById({ _id: bookId });
-        
+
         if (!book) res.status(404).json({ error: `No book with id: ${bookId}` });
+
+        res.status(200).json({ book });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// UPDATE BOOK BY ID
+router.put("/:id", async (req, res) => {
+    try {
+        const { id: bookId } = req.params;
+        const book = await Book.findByIdAndUpdate(
+            bookId,
+            {
+                name: req.body.bookName,
+                author: {
+                    name: req.body.authorName,
+                    age: req.body.authorAge,
+                },
+                genre: req.body.genre,
+            },
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        if (!book) {
+            res.status(404).json({ error: `No book with id: ${bookId}` });
+        }
 
         res.status(200).json({ book });
     } catch (error) {
